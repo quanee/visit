606428acc5d15377e384b50d657639b00c66a3b9
quanee <qy867112348@gmail.com>
2022-08-20 17:03:30 +0800

    update

--- main.go
+++ main.go
@@ -1,9 +1,56 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"log"
+	"net"
+)
+
 func (n *Node) Ping()    {}
 func (n *Node) Pong()    {}
 func (n *Node) Publish() {}
+func (n *Node) TickMsg() {
+	go n.tickMsg()
+}
 
 func main() {
+	n := &Node{
+		ctx:      context.Background(),
+		nodes:    make(map[string]*Node),
+		msgChan:  make(chan *Msg, 10000),
+		interval: 5,
+	}
+	var mip, mport, lip, lport string
+	var meeted bool
+	flag.StringVar(&mip, "mip", "127.0.0.1", "current node first meet cluster node's ip")
+	flag.StringVar(&mport, "mport", "63999", "current node first meet cluster node's port")
+	flag.StringVar(&lip, "lip", "127.0.0.1", "current node local cluster node's ip")
+	flag.StringVar(&lport, "lport", "20493", "current node local cluster node's port")
+	flag.BoolVar(&meeted, "meeted", false, "current node is first node of cluster")
+	flag.Uint64Var(&n.interval, "interval", 5, "node sync interval second")
+	flag.Parse()
+	log.SetFlags(log.Lshortfile)
 
+	updaddr, err := net.ResolveUDPAddr("udp", lip+":"+lport)
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn, err := net.ListenUDP("udp", updaddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	n.conn = conn
+	log.Printf("listing at %v:%v", lip, lport)
+	if meeted {
+		if err := n.Meet(mip, mport); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	n.TickMsg()
+	n.ReadMsg()
+	n.ProcessMsg()
+	for {
+	}
 }
--- type.go
+++ type.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net"
+	"net/netip"
+	"sync"
+	"time"
+)
+
 type State uint8
 
 const (
@@ -14,22 +24,153 @@ const (
 	_ MsgType = iota
 	PING
 	PONG
-	PUBLISH
 	MEET
+	MOOT
+	SYNC
+	PUBLISH
 )
+
+type Msg struct {
+	MsgType
+	AddrPort netip.AddrPort
+	Payload  []byte `json:"payload"`
+}
+
+type MeetMSG struct {
+	IP   string `json:"ip"`
+	Port int    `json:"port"`
+}
+
+type MootMSG struct {
+	ID string `json:"id"`
+}
 
 type Node struct {
 	status    State
-	id        uint64
+	ID        string `json:"id"`
 	fadeout   uint64
 	count     uint64
 	interval  uint64 // s
 	syncIndex uint64
 	nodes     map[string]*Node
 	ip        string
-	port      string
+	port      uint16
+	ctx       context.Context
+	mux       sync.RWMutex
+	conn      *net.UDPConn
+	msgChan   chan *Msg
 }
 
-type Msg struct {
-	payload []byte
+func (n *Node) tickMsg() {
+	ticker := time.NewTicker(time.Duration(n.interval) * time.Second)
+	for {
+		select {
+		case <-n.ctx.Done():
+			return
+		case <-ticker.C:
+			for _, node := range n.nodes {
+				_ = node
+			}
+		}
+	}
+}
+
+func (n *Node) ReadMsg() {
+	go n.readMsg()
+}
+
+func (n *Node) readMsg() {
+	for {
+		body := make([]byte, 4096)
+		length, raddr, err := n.conn.ReadFromUDPAddrPort(body)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		log.Println(raddr)
+		msg := &Msg{}
+		if err := json.Unmarshal(body[:length], msg); err != nil {
+			log.Println(err)
+			continue
+		}
+		msg.AddrPort = raddr
+		n.msgChan <- msg
+	}
+}
+
+func (n *Node) ProcessMsg() {
+	go n.processMsg()
+}
+
+func (n *Node) processMsg() {
+	for {
+		select {
+		case msg := <-n.msgChan:
+			switch msg.MsgType {
+			case MEET:
+				log.Printf("read meet msg: %v:%v\n", msg.AddrPort.Addr().String(), msg.AddrPort.Port())
+				if err := n.addNode(msg); err != nil {
+					log.Println(err)
+					continue
+				}
+			case MOOT:
+				moot := MootMSG{}
+				if err := json.Unmarshal(msg.Payload, &moot); err != nil {
+					log.Println(err)
+					continue
+				}
+				n.ID = moot.ID
+				log.Println("add successful, id: ", n.ID)
+			case PING:
+			case PONG:
+			case PUBLISH:
+			case SYNC:
+			}
+		}
+	}
+}
+
+func (n *Node) addNode(msg *Msg) error {
+	id := uuid()
+	n.nodes[id] = &Node{
+		ip:   msg.AddrPort.Addr().String(),
+		port: msg.AddrPort.Port(),
+	}
+	moot := MootMSG{}
+	moot.ID = id
+	body, err := json.Marshal(moot)
+	if err != nil {
+		return err
+	}
+	rmsg := Msg{
+		MsgType: MOOT,
+		Payload: body,
+	}
+	response, err := json.Marshal(rmsg)
+	if err != nil {
+		return err
+	}
+	if _, err := n.conn.WriteToUDPAddrPort(response, msg.AddrPort); err != nil {
+		return err
+	}
+
+	log.Printf("add node successful, id: %v", moot.ID)
+	return nil
+}
+
+func (n *Node) Meet(mip, mport string) error {
+	msg := Msg{MsgType: MEET}
+	body, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	udpaddr, err := net.ResolveUDPAddr("udp", mip+":"+mport)
+	if err != nil {
+		return err
+	}
+
+	if _, err := n.conn.WriteToUDP(body, udpaddr); err != nil {
+		return err
+	}
+	return nil
 }
